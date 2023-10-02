db4f23953848351b2ee2d2596a7afdc62971ac6d
GSinghh <Gbassans@yahoo.com>
2023-10-01 23:46:07 -0700

    created header

---
+++ script.js
@@ -0,0 +1,8 @@
+class Book {
+    constructor(title, author, numPages, finished) {
+            this.title = title;
+            this.author = author;
+            this.numPages = numPages;
+            this.finished = finished;
+    };
+}
