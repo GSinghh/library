5897ef4e54c3ff366967b61ca77159af12764632
GSinghh <Gbassans@yahoo.com>
2023-10-03 18:55:55 -0700

    Finished Modal

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ class Book {
 }
 
 const myLibrary = [];
+const openDialogButton = document.getElementById('open-dialog');
+const dialog = document.getElementById('dialog-input');
+const bookName = document.getElementById('name');
+const authorName = document.getElementById('author');
+const totalPages = document.getElementById('pages');
+const submit = document.getElementById('submit');
 
-const addBook = (Book) =>
-{
+const addBook = (Book) => {
     myLibrary.push(Book);
 }
+
+
+openDialogButton.addEventListener("click", () => {
+    
+    dialog.showModal();
+});
+
+
